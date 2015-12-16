d96a4a832ebf8fb40cd7c091a00e08ef40da52b2
sbfnk <sbfnk@users.noreply.github.com>
2015-12-16 17:47:58 +0000

    return libbi object when sampling observations

--- R/sample_observations.R
+++ R/sample_observations.R
@@ -2,11 +2,11 @@
 #' @name sample_observations
 #' @title Sample observations from trajectories of a libbi model, one at each time step
 #' @param wrapper a \code{\link{libbi}} object that has been run
-#' @param output_file_name the name of the output file to write
 #' @param ... further options to be passed to \code{libbi} (with run = TRUE)
+#' @return \code{\link{libbi}} object with sampled observations
 #' @export
 #' 
-sample_observations <- function(wrapper, output_file_name, ...){
+sample_observations <- function(wrapper, ...){
   if (!wrapper$run_flag) {
     stop("The model should be run first")
   }
@@ -22,20 +22,21 @@ sample_observations <- function(wrapper, output_file_name, ...){
   model$remove_block("initial")
 
   run_joint <- wrapper$clone(run = TRUE, model = model, 
-                             add_options = list("start-time" = start_time, 
-                                                "end-time" = end_time, 
-                                                "noutputs" = end_time - start_time, 
-                                                target = "joint",
-                                                nsamples = iterations),
+                             "start-time" = start_time, 
+                             "end-time" = end_time, 
+                             "noutputs" = end_time - start_time, 
+                             target = "joint",
+                             nsamples = iterations,
                              input = wrapper, init = wrapper,
-                             output_file_name = output_file_name, ...)
+                             ...)
 
-  res <- bi_read(output_file_name)
+  res <- bi_read(run_joint$result$output_file_name)
   for (var in names(res)) {
       if ("nr" %in% names(res[[var]])) {
           res[[var]]$nr <- res[[var]]$nr - 1
           res[[var]] <- res[[var]][res[[var]]$nr >= 0, ]
       }
   }
-  bi_write(output_file_name, res)
+  bi_write(run_joint$result$output_file_name, res)
+  return(run_joint)
 }
