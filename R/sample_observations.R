201b3a907ccb23f6d5930a2445be9ac9d9e2077f
Sebastian Funk <sbfnk@users.noreply.github.com>
2016-02-14 09:17:11 +0000

    sample_observation: reduce IO

--- R/sample_observations.R
+++ R/sample_observations.R
@@ -3,7 +3,7 @@
 #' @title Sample observations from trajectories of a libbi model, one at each time step
 #' @param wrapper a \code{\link{libbi}} object that has been run
 #' @param ... further options to be passed to \code{libbi} (with run = TRUE)
-#' @return \code{\link{libbi}} object with sampled observations
+#' @return \code{\link{libbi}} object with sampled observations (if an \code{output_file_name} has been given), or a list of data frames containing the results
 #' @export
 #' 
 sample_observations <- function(wrapper, ...){
@@ -21,10 +21,10 @@ sample_observations <- function(wrapper, ...){
   model$remove_block("parameter")
   model$remove_block("initial")
 
-  run_joint <- wrapper$clone(run = TRUE, model = model, 
-                             "start-time" = start_time, 
-                             "end-time" = end_time, 
-                             "noutputs" = end_time - start_time, 
+  run_joint <- wrapper$clone(run = TRUE, model = model,
+                             "start-time" = start_time,
+                             "end-time" = end_time,
+                             "noutputs" = end_time - start_time,
                              target = "joint",
                              nsamples = iterations,
                              input = wrapper, init = wrapper,
@@ -37,6 +37,14 @@ sample_observations <- function(wrapper, ...){
           res[[var]] <- res[[var]][res[[var]]$nr >= 0, ]
       }
   }
-  bi_write(run_joint$result$output_file_name, res)
-  return(run_joint)
+  ## if an output file has been given, we re-write
+  args <- match.call()
+  if ("output_file_name" %in% names(args))
+  {
+      bi_write(run_joint$result$output_file_name, res)
+      return(run_joint)
+  } else
+  {
+      return(res)
+  }
 }
