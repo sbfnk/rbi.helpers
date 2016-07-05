34dd51d257b0cf551f62fb44d69ed14c58cd8e7a
sbfnk <sbfnk@users.noreply.github.com>
2016-07-05 15:22:07 +0100

    full adaptive MCMC

--- R/output_to_proposal.R
+++ R/output_to_proposal.R
@@ -18,22 +18,42 @@ output_to_proposal <- function(wrapper, scale) {
   params <- model$get_vars("param")
   res <- bi_read(wrapper$result$output_file_name, vars = params)
 
+  l <- lapply(names(res), function(x) {
+    y <- copy(res[[x]])
+    setnames(y, "value", x)
+    y
+  })
+
+  if (length(l) > 1) {
+    wide <- l[[1]]
+    for (i in seq(2, length(l))) {
+      wide <- merge(wide, l[[i]])
+    }
+  } else {
+    wide <- l
+  }
+
+  C <- cov(wide)
+  sd_vec <- sqrt(diag(C) - C[1, ]**2 / C[1, 1])
+  mean_scale <- C[1, ] / C[1, 1]
+
   if (missing(scale)) {
     scale_string <- ""
   } else {
     scale_string <- paste0(scale, " * ")
   }
 
-  param_sd <- sapply(params, function(p) {
-      ifelse(length(res[[p]]) == 1, 0, sd(res[[p]]$value))
-  })
-
   param_block <- model$get_block("parameter")
   param_bounds <- sapply(params, function(param) {grep(paste0("^[[:space:]]*", param, "[[[:space:]][^~]*~"), param_block, value = TRUE)})
   variable_bounds <- param_bounds[sapply(param_bounds, function(x) {length(x) > 0})]
 
-  proposal_lines <- unname(sapply(names(variable_bounds), function(param) {
-      param_string <-
+  proposal_lines <- c()
+
+  first <- TRUE
+  first_param_string <- ""
+  first_param_diff <- ""
+  for (param in names(variable_bounds)) {
+    param_string <-
         sub(paste0("^[:space:]*(", param, "[^[:space:]~]*)[[:space:]~].*$"), "\\1", variable_bounds[param])
     param_bounds_string <-
       sub("^.*(uniform|truncated_gaussian|truncated_normal)\\(([^\\)]+)\\).*$",
@@ -44,11 +64,25 @@ output_to_proposal <- function(wrapper, scale) {
     dist <- args[[1]][1]
     bounds_string <- args[[1]][2]
 
+    if (first) {
+      old_name <- paste0(param_string, "_")
+      first_param_diff <- paste0("(", param_string, " - ", old_name, ")")
+      proposal_lines <- c(paste0("param ", old_name, "(has_output = 0)"),
+                          paste(old_name, "<-", param_string))
+      mean <- param_string
+      sd <- C[param_string, param_string]
+      first <- FALSE
+    } else {
+      mean <- paste0(param_string, " + (", mean_scale[param_string], ") * ", first_param_diff)
+      sd <- sd_vec[param_string]
+    }
+
     if (is.na(bounds_string) || bounds_string == variable_bounds[param]) {
-      paste0(param_string, " ~ gaussian(",
-             "mean = ", param_string,
-             ", std = ", scale_string,
-             ifelse(param_sd[param] > 0, param_sd[param], 1), ")")
+      proposal_lines <-
+        c(proposal_lines,
+          paste0(param_string, " ~ gaussian(",
+                 "mean = ", mean,
+                 ", std = ", scale_string, sd, ")"))
     } else {
       bounds <- c(lower = NA, upper = NA)
 
@@ -83,7 +117,7 @@ output_to_proposal <- function(wrapper, scale) {
       eval_bounds <- tryCatch(
       {
           sapply(bounds, function(x) { eval(parse(text = x))})
-      }, 
+      },
       error = function(cond)
       {
           warning("Cannot convert bounds for ", param, "into R expression")
@@ -94,26 +128,27 @@ output_to_proposal <- function(wrapper, scale) {
       })
       bounds <- eval_bounds
 
-      if (param_sd[param] == 0) {
+      if (sd_vec[param] == 0) {
         ## no variation
         if (sum(!is.na(is.numeric(bounds))) == 2) {
-          ## range / 10 
-          param_sd[param] <- diff(as.numeric(bounds)) / 10
+          ## range / 10
+          sd_vec[param] <- diff(as.numeric(bounds)) / 10
         } else {
-          param_sd[param] <- 1
+          sd_vec[param] <- 1
         }
       }
 
-      paste0(param_string, " ~ truncated_gaussian(",
-             "mean = ", param_string,
-             ", std = ", scale_string, param_sd[param], 
-             ifelse(length(bounds) > 0,
-                    paste0(", ", paste(names(bounds), "=", bounds,
-                                       sep = " ", collapse = ", "),
-                           ")"),
-                    ")"))
+      proposal_lines <- c(proposal_lines,
+                          paste0(param_string, " ~ truncated_gaussian(",
+                                 "mean = ", mean, 
+                                 ", std = ", scale_string, sd, 
+                                 ifelse(length(bounds) > 0,
+                                        paste0(", ", paste(names(bounds), "=", bounds,
+                                                           sep = " ", collapse = ", "),
+                                               ")"),
+                                        ")")))
     }
-  }))
+  }
 
   model$add_block(name = "proposal_parameter",
                   lines = proposal_lines)
