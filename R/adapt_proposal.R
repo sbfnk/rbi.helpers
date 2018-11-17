c5aab0cea5288879641abdff214b5e0f3450e908
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2018-11-17 12:42:31 +0000

    adapt_proposal: update scale

--- R/adapt_proposal.R
+++ R/adapt_proposal.R
@@ -127,7 +127,7 @@ adapt_proposal <- function(x, min = 0, max = 1, scale = 2, max_iter = 10, adapt 
       }
 
       adaptation_vars <-
-        output_to_cov(adapted, correlations = (round == 2))
+        output_to_cov(adapted, scale=adapt_scale, correlations = (round == 2))
       sample_opts <- list(x=adapted)
       if ("input-file" %in% names(x$options)) {
         bi_write(x$options[["input-file"]], adaptation_vars, append=TRUE)
