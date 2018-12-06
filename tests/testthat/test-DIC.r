e6aa9226a16d803d42984ebd88858191c445318e
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2018-12-06 20:38:41 +0000

    better DIC test

--- tests/testthat/test-DIC.r
+++ tests/testthat/test-DIC.r
@@ -1,9 +1,8 @@
 context("Testing functions for information criteria")
 
-##' example_run <- bi_read(system.file(package="rbi", "example_output.nc"))
-##' example_model_file <- system.file(package="rbi", "PZ.bi")
-##' example_bi <- attach_data(libbi(example_model_file), "output", example_run)
-##' DIC(example_bi)
+example_run <- bi_read(system.file(package="rbi", "example_output.nc"))
+example_model_file <- system.file(package="rbi", "PZ.bi")
+example_bi <- attach_data(libbi(example_model_file), "output", example_run)
 
 test_that("We can calculate DIC",
 {
