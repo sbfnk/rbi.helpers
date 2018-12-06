072fa7d228b031d2c22937d715aeba15f5d7f33b
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2018-12-06 20:20:32 +0000

    acceptance_rate: man page update

--- R/acceptance_rate.R
+++ R/acceptance_rate.R
@@ -9,9 +9,9 @@
 #' @importFrom rbi get_traces
 #' @export
 #' @examples
-#' example_run_file <- system.file(package="rbi.helpers", "example_run.nc")
+#' example_run <- bi_read(system.file(package="rbi.helpers", "example_run.nc"))
 #' example_model_file <- system.file(package="rbi", "PZ.bi")
-#' example_bi <- attach_data(libbi(example_model_file), "output", example_run_file)
+#' example_bi <- attach_data(libbi(example_model_file), "output", example_run)
 #' acceptance_rate(example_bi)
 #' @author Sebastian Funk
 acceptance_rate <- function(...) {
