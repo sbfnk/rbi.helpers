5368f1cf96aa5a103ef4fef042f5c9e253fdd72b
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2018-12-09 23:57:15 +0000

    update unit tests

--- tests/testthat/test-adapt.r
+++ tests/testthat/test-adapt.r
@@ -2,11 +2,41 @@ context("Testing functions for adapting")
 
 model <- system.file(package="rbi", "PZ.bi")
 example_output <- bi_read(system.file(package="rbi", "example_output.nc"))
-bi <- libbi(model)
+bi <- libbi(model, nparticles=1)
 data <- bi_generate_dataset(bi, seed=1234)
 
-test_that("the number of particles can be adapted",
+test_that("pMCMC adaptation works",
 {
-  expect_true(adapt_particles(bi, obs=data, nsamples=100, seed=1234)$options$nparticles>0)
-  expect_gt(length(bi_read(adapt_proposal(bi, obs=data, nsamples=100, min=0.1, max=0.52, adapt="both", seed=1234), file="input")), 0)
+  capture.output(
+    prop_adapted <-
+      adapt_proposal(bi, obs=data, nsamples=100, min=0.5, max=0.53,
+                     adapt="both", seed=3)
+  )
+  prop_adapted2 <-
+    adapt_proposal(prop_adapted, with="transform-initial-to-param", min=0.4, max=0.5, adapt="shape", seed=5, scale=0.5, quiet=TRUE)
+  capture.output(part_adapted <- adapt_particles(prop_adapted, seed=1))
+  capture.output(part_adapted2 <- adapt_particles(bi, seed=1, obs=data))
+  expect_true(part_adapted$options$nparticles>0)
+  expect_gt(length(bi_read(prop_adapted, file="input")), 0)
+  expect_gt(length(bi_read(adapt_proposal(prop_adapted, min=0, max=1), file="input")), 0)
+})
+
+test_that("warnings are given",
+{
+  expect_warning(adapt_proposal(bi, size=TRUE, adapt="shape"), "adapt size")
+  expect_warning(adapt_proposal(bi, correlations=TRUE, adapt="size"), "adapt shape")
+})
+
+test_that("errors are reported",
+{
+    expect_error(acceptance_rate(data), "just one sample")
+    expect_error(adapt_particles(bi, min=1, max=0), "greater")
+    expect_error(adapt_proposal(bi, min=1, max=0), "max>min")
+    expect_error(adapt_proposal(bi, adapt="test"), "one of")
+})
+
+test_that("deprecated options are recognised",
+{
+  expect_warning(adapt_proposal(bi, size=TRUE), "adapt")
+  expect_warning(adapt_proposal(bi, correlations=TRUE), "adapt")
 })
---
+++ tests/testthat/test-proposal.r
@@ -0,0 +1,72 @@
+context("Testing generating multivariate proposals")
+
+model_str <- "
+model test {
+  const no_a = 2
+  const no_b = 3
+  dim a(no_a)
+  dim b(no_b)
+
+  obs M[a]
+
+  state N[a] (has_input = 0)
+  noise e[a, b]
+  param m[a, b]
+
+  sub parameter {
+    m[0,0] ~ beta()
+    m[0,1] ~ gamma()
+    m[0,2] ~ gaussian(mean=1)
+    m[1,b] ~ truncated_gaussian(mean=1, lower=m[0,b])
+  }
+
+  sub initial {
+    N[a] <- 1
+  }
+
+  sub transition {
+    e[a, b] ~ gaussian(mean = m[a,b])
+    N[a] <- N[a] + e[a, 0] +
+       e[a, 1]
+  }
+
+  sub observation {
+    inline x = m
+    M[a] ~ gaussian(mean = N[a])
+  }
+}
+"
+
+model <- bi_model(lines = stringi::stri_split_lines(model_str)[[1]])
+bi <- libbi(model, dims=list(a=c("first", "second")))
+test_output <-
+    Map(
+        function(x) { if (is.data.frame(x)) x$value <- abs(rnorm(nrow(x))); x },
+        list(e=data.frame(expand.grid(time=0:1, a=c("first", "second"), b=0:1, np=0:1)),
+             N=data.frame(expand.grid(time=0:1, a=c("first", "second"), np=0:1)),
+             m=data.frame(expand.grid(a=c("first", "second"), b=0:1, np=0:1)),
+             close=0,
+             loglikelihood=data.frame(np=0:1),
+             logprior=data.frame(np=0:1)))
+bi <- attach_data(bi, "output", test_output)
+
+test_that("multivariate proposals can be generated", 
+{
+  updated_model <- update_proposal(model)
+  bi$model <- updated_model
+
+  expect_gt(length(get_mvn_params(bi)), 0)
+  expect_gt(length(get_mvn_params(bi, fix=0)), 0)
+
+  test_output2 <- test_output
+  test_output2$m$value <- 1
+  bi2 <- attach_data(bi, "output", test_output2)
+
+  expect_gt(length(get_mvn_params(bi2)), 0)
+
+})
+
+test_that("errors are reported",
+{
+    expect_error(update_proposal(3), "must be a")
+})
