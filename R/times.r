3d3ed0f90b8923b9ae2a2d7f600750b146eb86a1
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2018-12-14 08:48:16 +0000

    another rename

--- R/times.r
+++ R/times.r
@@ -20,23 +20,22 @@ split_unit <- function(unit_string) {
   return(list(num=amount, unit=unit))
 }
 
-#' @rdname to_actual_times
-#' @name to_actual_times
-#' @title Convert LibBi times to actual times or dates
-#' @description This function converts from LibBi times (i.e., 0, 1, 2, ...) to actual times or dates
+#' @rdname numeric_to_times
+#' @name numeric_to_times
+#' @title Convert numeric times to actual times or dates
+#' @description This function converts from numeric times (i.e., 0, 1, 2, ...) to actual times or dates
 #' @param x a \code{\link{libbi}} object which has been run, or a list of data
 #'     frames containing state trajectories (as returned by \code{bi_read})
-#' @param origin the time origin, i.e. the date or time corresponding to LibBi
-#'   time 0
-#' @param unit the unit of time that each time step corresponds to; this must bg
+#' @param origin the time origin, i.e. the date or time corresponding to time 0
+#' @param unit the unit of time that each time step corresponds to; this must be
 #'   a unit understood by \code{lubridate::period}, optionally with a number in
 #'   advance, e.g. "day" or "2 weeks" or "3 seconds"
 #' @param ... any arguments for \code{bi_read} (e.g., \code{file})
-#' @return a list of data frames as returned bi \code{bi_read}, but with real times
+#' @return a list of data frames as returned by \code{bi_read}, but with real times
 #' @importFrom lubridate period
 #' @importFrom rbi bi_read
 #' @export
-to_actual_times <- function(x, origin, unit, ...) {
+numeric_to_times <- function(x, origin, unit, ...) {
 
   if (("libbi" %in% class(x)) || (is.character(x))) {
     vars <- do.call(bi_read, list(x=x, ...))
@@ -60,20 +59,19 @@ to_actual_times <- function(x, origin, unit, ...) {
   return(vars)
 }
 
-#' @rdname to_libbi_times
-#' @name to_libbi_times
-#' @title Convert actual times or dates to LibBi times
-#' @description This function converts from real times/dates to LibBi times
+#' @rdname times_to_numeric
+#' @name times_to_numeric
+#' @title Convert actual times or dates to numeric times
+#' @description This function converts from real times/dates to numeric times (0, 1, 2, ...)
 #' @param x a data frame containing a "time" column, or a list containing such data frames
-#' @param origin the time origin, i.e. the date or time corresponding to LibBi
-#'   time 0
-#' @param unit the unit of time that each time step corresponds to; this must bg
+#' @param origin the time origin, i.e. the date or time corresponding to time 0
+#' @param unit the unit of time that each time step corresponds to; this must be
 #'   a unit understood by \code{lubridate::period}, optionally with a number in
 #'   advance, e.g. "day" or "2 weeks" or "3 seconds"
-#' @return a list of data frames that can be passed to libbi
+#' @return a list of data frames that can be passed to \code{libbi}
 #' @importFrom lubridate as.interval period
 #' @export
-to_libbi_times <- function(x, origin, unit) {
+times_to_numeric <- function(x, origin, unit) {
 
   if (is.data.frame(x)) {
     vars <- list(df=x)
@@ -82,7 +80,7 @@ to_libbi_times <- function(x, origin, unit) {
     vars <- x
     list_given <- TRUE
   } else {
-    stop("'x' must be a data frame or a list of data frames.")
+    stop("'x' must be a data frame or a list.")
   }
 
   ## convert unit string to time step for lubridate::period
